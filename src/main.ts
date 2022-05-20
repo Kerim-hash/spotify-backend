a2f70fdb78c71b1e21db6c01b39580be2e408633
Kerim <kerim.nurkaliev@sibers.com>
2022-05-20 17:38:56 +0600

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(ConfigService.get('PORT') || 5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
